a66ef8138030ac3b0a881e3659e48c3bd7dab425
asnimpj <asnim96@gmail.com>
2017-07-18 22:15:00 +0530

    Branch Choosing Changed to server Type

---
+++ app/src/main/java/com/example/vignesh/ktu/extra/CusUtils.java
@@ -0,0 +1,23 @@
+package com.example.vignesh.ktu.extra;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by asnim on 27/05/17.
+ */
+
+
+public class CusUtils {
+    private static FirebaseDatabase mDatabase;
+
+    public static FirebaseDatabase getDatabase() {
+        if (mDatabase == null) {
+            mDatabase = FirebaseDatabase.getInstance();
+            mDatabase.setPersistenceEnabled(true);
+
+//
+        }
+        return mDatabase;
+    }
+
+}
--- app/src/main/java/com/example/vignesh/ktu/models/Branch.java
+++ app/src/main/java/com/example/vignesh/ktu/models/Branch.java
@@ -1,5 +1,7 @@
 package com.example.vignesh.ktu.models;
 
+import android.util.Log;
+
 /**
  * Created by asnim on 18/07/17.
  */
@@ -7,5 +9,16 @@ package com.example.vignesh.ktu.models;
 public class Branch {
     public String branch_name;
     public String branch_code;
-    public String branch_semesters;
+    public int branch_semesters;
+
+    public void printBranchDetails(){
+        Log.e("B_NAME",this.branch_name);
+        Log.e("B_CODE",this.branch_code);
+        Log.e("B_SEM",this.branch_semesters+"");
+
+    }
+    public Branch(){
+
+    }
+
 }
