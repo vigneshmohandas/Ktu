d5d4727aa86abc902ee4964eb896a45fae925277
asnimpj <asnim96@gmail.com>
2017-07-18 21:10:48 +0530

    Added Models for DB

---
+++ app/src/main/java/com/example/vignesh/ktu/models/Branch.java
@@ -0,0 +1,11 @@
+package com.example.vignesh.ktu.models;
+
+/**
+ * Created by asnim on 18/07/17.
+ */
+
+public class Branch {
+    public String branch_name;
+    public String branch_code;
+    public String branch_semesters;
+}
---
+++ app/src/main/java/com/example/vignesh/ktu/models/Semester.java
@@ -0,0 +1,10 @@
+package com.example.vignesh.ktu.models;
+
+/**
+ * Created by asnim on 18/07/17.
+ */
+
+public class Semester {
+    public String semester_name;
+    public String semester_code;
+}
