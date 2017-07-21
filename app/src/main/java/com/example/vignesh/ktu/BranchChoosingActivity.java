8b26d770f07cb74446d79fba29bed37197c6c47f
vigneshmohandas <vigneshmohandas123@gmail.com>
2017-07-21 20:54:47 +0530

    hi

--- app/src/main/java/com/example/vignesh/ktu/BranchChoosingActivity.java
+++ app/src/main/java/com/example/vignesh/ktu/BranchChoosingActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.vignesh.ktu.extra.CusUtils;
 import com.example.vignesh.ktu.models.Branch;
+import com.example.vignesh.ktu.models.Semester;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +19,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static com.example.vignesh.ktu.R.id.parent;
 
 public class BranchChoosingActivity extends AppCompatActivity {
     ArrayList<Branch> branchArrayList;
@@ -31,14 +35,14 @@ public class BranchChoosingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch);
-        branchListView = (ListView)findViewById(R.id.branchListView);
+        branchListView = (ListView) findViewById(R.id.branchListView);
         databaseReference = CusUtils.getDatabase().getReference().child("btech");
         databaseReference.keepSynced(true);
         branchArrayList = new ArrayList<>();
         branchHashMap = new HashMap<>();
-        Log.e("WHERE","BRANCH CHOOSER");
+        Log.e("WHERE", "BRANCH CHOOSER");
 
-        Log.e("DB_REF",(databaseReference == null)+"");
+        Log.e("DB_REF", (databaseReference == null) + "");
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -46,17 +50,17 @@ public class BranchChoosingActivity extends AppCompatActivity {
                 Iterable<DataSnapshot> dataSnapshotIterable = dataSnapshot.getChildren();
                 branchdisplay = new ArrayList<String>();
 
-                Log.e("CHILDRED",dataSnapshot.getChildrenCount()+"");
-                for (DataSnapshot branch: dataSnapshotIterable){
+                Log.e("CHILDRED", dataSnapshot.getChildrenCount() + "");
+                for (DataSnapshot branch : dataSnapshotIterable) {
                     Branch branch1 = branch.getValue(Branch.class);
 
 
                     branch1.printBranchDetails();
-                    branchHashMap.put(branch1.branch_name,branch1);
+                    branchHashMap.put(branch1.branch_name, branch1);
                     branchdisplay.add(branch1.branch_name);
 
                 }
-                branchadapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.list_layout,branchdisplay);
+                branchadapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_layout, branchdisplay);
                 branchListView.setAdapter(branchadapter);
 
             }
@@ -66,6 +70,13 @@ public class BranchChoosingActivity extends AppCompatActivity {
 
             }
         });
-    }
+        branchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i= new Intent(BranchChoosingActivity.this,SemesterChoosingActivity.class);
+                startActivity(i);
+            }
+        });
 
+    }
 }
--- app/src/main/java/com/example/vignesh/ktu/semester1.java
+++ app/src/main/java/com/example/vignesh/ktu/semester1.java
@@ -1,7 +1,9 @@
 package com.example.vignesh.ktu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class semester1 extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class semester1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semester1);
     }
+
 }
